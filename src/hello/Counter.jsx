2b6242cd5532918e2bc806541a6977e7d424cdf0
YunHyeRim1 <0305_08@naver.com>
2021-01-19 16:02:03 +0900

    first

---
+++ src/hello/Counter.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [ number, setNumber ] = useState(0)
+    return <>
+    <h1>카운터</h1>
+    <h2>{number}</h2>
+    <button onClick = { () => SVGAnimatedNumber(number + 1)}> + </button>
+    <button onClick = { () => SVGAnimatedNumber(number + 1)}> - </button>
+    </>
+}
+
+export default Counter
